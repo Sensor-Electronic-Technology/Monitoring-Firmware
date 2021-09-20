fa0aa4806e7174b3004014103f247f256192f280
Andrew Elmendorf <aelmendorf234@gmail.com>
2021-09-20 15:22:30 -0400

    fixed logger and action clearing

--- MonitoringController.cpp
+++ MonitoringController.cpp
@@ -3,6 +3,84 @@
 
 namespace MonitoringComponents {
 
+	void MonitoringController::Setup() {
+		MonitoringLogger::Start(&Serial, LogLevel::Info);
+		this->systemActionLatches.insert(std::pair<ActionType, bool>(ActionType::Alarm, false));
+		this->systemActionLatches.insert(std::pair<ActionType, bool>(ActionType::Warning, false));
+		this->systemActionLatches.insert(std::pair<ActionType, bool>(ActionType::SoftWarn, false));
+		this->systemActionLatches.insert(std::pair<ActionType, bool>(ActionType::Maintenance, false));
+		this->systemActionLatches.insert(std::pair<ActionType, bool>(ActionType::Okay, false));
+
+		for(auto action : this->actions) {
+			//this->actionTracking.insert(std::pair<int, Registrations*>(action->Id(), new Registrations));
+			this->tracking.insert(std::pair<int, int*>(action->Id(), new int(0)));
+		}
+
+		this->OnChannelCallback([&](ChannelMessage message) {
+				ProcessChannelMessage(message);
+			});
+
+		this->Build();
+
+		this->printTimer.onInterval([&]() {
+			String buffer;
+			MonitoringLogger::LogInfo(F("Latches"));
+			for(auto actionLatches : systemActionLatches) {
+				MonitoringLogger::LogInfo(F("ActionType: %u State: %u"), (int)actionLatches.first, actionLatches.second);
+				//cout << "ActionType: " << (int)actionLatches.first << " State: " << actionLatches.second << endl;
+			}
+
+			switch(controllerState) {
+			case ControllerState::Alarming:
+			{
+				//cout << "ControllerState: " << "Alarming" << endl;
+				MonitoringLogger::LogInfo(F("ControllerState: Alarming"));
+				break;
+			}
+
+			case ControllerState::Maintenance:
+			{
+				//cout << "ControllerState: " << "Maintenance" << endl;
+				MonitoringLogger::LogInfo(F("ControllerState: Maintenance"));
+				break;
+			}
+
+			case ControllerState::Warning:
+			{
+				//cout << "ControllerState: " << "Warning" << endl;
+				MonitoringLogger::LogInfo(F("ControllerState: Warning"));
+				break;
+			}
+
+			case ControllerState::Okay:
+			{
+				//cout << "ControllerState: " << "Okay" << endl;
+				MonitoringLogger::LogInfo(F("ControllerState: Okay"));
+				break;
+			}
+			}
+
+			//cout << "Action Registrations: " << endl;
+			MonitoringLogger::LogInfo(F("Action Registrations: "));
+			for(auto registration : tracking) {
+				MonitoringLogger::LogInfo(F("Id: %u Instances: %u"), registration.first, (*registration.second));
+			}
+			}, 1000);
+
+		MonitoringLogger::LogInfo(F("Latches"));
+		for(auto actionLatches : systemActionLatches) {
+			MonitoringLogger::LogInfo(F("ActionType: %u State: %u"), (int)actionLatches.first, actionLatches.second);
+		}
+
+		MonitoringLogger::LogInfo(F("Action Registrations: "));
+		for(auto registration : tracking) {
+			MonitoringLogger::LogInfo(F("Id: %u Instances: %u"), registration.first, (*registration.second));
+		}
+
+		RegisterChild(this->printTimer);
+		//RegisterChild(this->checkStateTimer);
+	}
+	
 	void MonitoringController::Build() {
 		ConfigurationReader reader;
 		reader.Init();
@@ -15,6 +93,7 @@ namespace MonitoringComponents {
 		auto actionConfig = reader.DeserializeActions();
 
 		auto netConfig = reader.DeserializeNetConfiguration();
+		MonitoringLogger::SetUpFileLogger();
 		ModbusService::Initialize(netConfig);
 
 		for (auto output : outputConfig) {
@@ -22,6 +101,7 @@ namespace MonitoringComponents {
 			this->outputChannels.push_back(channel);
 		}
 
+		MonitoringLogger::LogInfo(F("Creating Actions"));
 		for (int i = 0; i < actionConfig.size();i++) {
 			Action* action=new Action(actionConfig[i]);
 			
@@ -61,6 +141,8 @@ namespace MonitoringComponents {
 			this->actions.push_back(action);
 		}
 
+
+		MonitoringLogger::LogInfo(F("Creating Discrete Channels"));
 		for (auto ch : discreteConfig) {
 			DiscreteInputChannel* channel = new DiscreteInputChannel(ch);
 			this->discreteInputs.push_back(channel);
@@ -68,6 +150,7 @@ namespace MonitoringComponents {
 			channel->OnStateChange(this->_on_channel_cbk);
 		}
 
+		MonitoringLogger::LogInfo(F("Creating Analog Channels"));
 		for (auto ch : analogConfig) {
 			AnalogInputChannel* channel = new AnalogInputChannel(ch);
 			this->analogInputs.push_back(channel);
@@ -78,79 +161,6 @@ namespace MonitoringComponents {
 
 	void MonitoringController::OnChannelCallback(ChannelCallback cbk) {
 		this->_on_channel_cbk = cbk;
-	}
-
-	void MonitoringController::Setup() {
-		this->systemActionLatches.insert(std::pair<ActionType, bool>(ActionType::Alarm, false));
-		this->systemActionLatches.insert(std::pair<ActionType, bool>(ActionType::Warning, false));
-		this->systemActionLatches.insert(std::pair<ActionType, bool>(ActionType::SoftWarn, false));
-		this->systemActionLatches.insert(std::pair<ActionType, bool>(ActionType::Maintenance, false));
-		this->systemActionLatches.insert(std::pair<ActionType, bool>(ActionType::Okay, false));
-
-		for(auto action : this->actions) {
-			//this->actionTracking.insert(std::pair<int, Registrations*>(action->Id(), new Registrations));
-			this->tracking.insert(std::pair<int, int*>(action->Id(), new int(0)));
-		}
-
-		this->OnChannelCallback([&](ChannelMessage message) {
-				ProcessChannelMessage(message);
-			});
-		
-		this->Build();
-
-		this->printTimer.onInterval([&]() {
-			String buffer;
-			MonitoringLogger::LogInfo(F("Latches"));
-			for (auto actionLatches : systemActionLatches) {
-				MonitoringLogger::LogInfo(F("ActionType: %u State: %u"), (int)actionLatches.first, actionLatches.second);
-				//cout << "ActionType: " << (int)actionLatches.first << " State: " << actionLatches.second << endl;
-			}
-
-			switch(controllerState) {
-				case ControllerState::Alarming: {
-					//cout << "ControllerState: " << "Alarming" << endl;
-					MonitoringLogger::LogInfo(F("ControllerState: Alarming"));
-					break;
-				}
-
-				case ControllerState::Maintenance:{
-					//cout << "ControllerState: " << "Maintenance" << endl;
-					MonitoringLogger::LogInfo(F("ControllerState: Maintenance"));
-					break;
-				}
-
-				case ControllerState::Warning:{
-					//cout << "ControllerState: " << "Warning" << endl;
-					MonitoringLogger::LogInfo(F("ControllerState: Warning"));
-					break;
-				}
-
-				case ControllerState::Okay:{
-					//cout << "ControllerState: " << "Okay" << endl;
-					MonitoringLogger::LogInfo(F("ControllerState: Okay"));
-					break;
-				}
-			}
-
-			//cout << "Action Registrations: " << endl;
-			MonitoringLogger::LogInfo(F("Action Registrations: "));
-			for(auto registration : tracking) {
-				MonitoringLogger::LogInfo(F("Id: %u Instances: %u"), registration.first, (*registration.second));
-			}
-		}, 1000);
-
-		MonitoringLogger::LogInfo(F("Latches"));
-		for(auto actionLatches : systemActionLatches) {
-			MonitoringLogger::LogInfo(F("ActionType: %u State: %u"), (int)actionLatches.first, actionLatches.second);
-		}
-
-		MonitoringLogger::LogInfo(F("Action Registrations: "));
-		for(auto registration : tracking) {
-			MonitoringLogger::LogInfo(F("Id: %u Instances: %u"), registration.first, (*registration.second));
-		}
-		
-		RegisterChild(this->printTimer);
-		//RegisterChild(this->checkStateTimer);
 	}
 
 	void MonitoringController::Initialize() {
@@ -180,7 +190,8 @@ namespace MonitoringComponents {
 		});
 
 		if(action == actions.end()) {
-			MonitoringLogger::LogError(F("Invalid ActionId: %d  From Channel(Slot,Channel): (%d,%d)"),message.actionId,message.channel.slot,message.channel.channel);
+			MonitoringLogger::LogError(F("Invalid ActionId: %d  From Channel(Slot,Channel): (%d,%d)"),
+				message.actionId,message.channel.slot,message.channel.channel);
 			return;
 		}
 
@@ -202,6 +213,7 @@ namespace MonitoringComponents {
 				if(message.type == ActionType::Custom) {
 					(*action)->Clear();
 				} else {
+					(*actionCount) -= 1;
 					this->systemActionLatches[message.type] = false;
 					this->ProcessStateChanges();
 				}
@@ -223,8 +235,6 @@ namespace MonitoringComponents {
 	}
 
 	void MonitoringController::ProcessStateChanges() {
-
-
 		if (systemActionLatches[ActionType::Maintenance]) {
 			if (this->controllerState != ControllerState::Maintenance) {
 				this->controllerState = ControllerState::Maintenance;
--- MonitoringFirmware.ino
+++ MonitoringFirmware.ino
@@ -29,6 +29,7 @@ void(*resetFunc)(void) = 0;
 bool resetLatched;
 
 void setup(){
+    //MonitoringLogger::Start();
 #if CREATEFILES==1
     Serial.begin(38400);
     while (!Serial) { ; }
