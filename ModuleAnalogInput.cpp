84c1dfe69d8e391851899a9697aeb6a9ae25040e
Andrew Elmendorf <aelmendorf234@gmail.com>
2022-01-26 14:10:07 -0500

    d

--- ModuleAnalogInput.cpp
+++ ModuleAnalogInput.cpp
@@ -7,11 +7,15 @@ namespace MonitoringComponents {
 		this->value = 20;
 		val = 20;
 #else
-		float counts = P1.readAnalog(this->_address.slot, this->_address.channel);
+/* 		float counts = P1.readAnalog(this->_address.slot, this->_address.channel);
 		 val= CurrentMax * ((float)counts / Bit13Reg);
-		this->value = val;
+		this->value = val; */
+		for(int i=0;i<10;i++){
+			int counts = P1.readAnalog(this->_address.slot, this->_address.channel);
+		 	val+= CurrentMax * ((float)counts / Bit13Reg);
+		}
+		this->value = val/10;
 #endif
-
 		return val;
 	}
 
